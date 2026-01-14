c55c0e3f0ef26c9c8ba34b175b4858c1e9a02b6c
Eslam Al-Okda <al3kda1999@gmail.com>
2026-01-14 04:22:16 +0200

    create a user profile card using props - task

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import WelcomeMessage from './components/welcomeMessage'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import MainContent from './components/MainContent'
+import UserProfile from './components/UserProfile'
 
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
     <WelcomeMessage />
     <Header />
     <MainContent />
+    <UserProfile name="Eslam" age="26" bio="Frontend Developer"/>
     <Footer />
     </>
   )
