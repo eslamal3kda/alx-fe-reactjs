b8adc98b22dfc50e93e314079b6d7d30ed3a655d
Eslam Al-Okda <al3kda1999@gmail.com>
2026-01-17 02:17:20 +0200

    0. Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer style={{backgroundColor:'navy', color:'white',textAlign:'center',padding:'20px',fontSize:'20px'}}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MainContent() {
+    return (
+        <main style={{color:'green',fontSize:'20px',padding:'10px',backgroundColor:'#EEE',borderRadius:'20px'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
