8df9f66289685c7db4a4b4912f0decc7010e08e7
Eslam Al-Okda <al3kda1999@gmail.com>
2026-01-22 05:41:11 +0200

    0. Initialize the Recipe Sharing Application with Zustand State Management

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+
+export default function App() {
+    return (
+        <>
+            <RecipeList />
+            <AddRecipeForm />
+        </>
+    );
+}
