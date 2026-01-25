7dc9e7675fb2972b7a095a91bdeb4c7c40bc188e
Eslam Al-Okda <al3kda1999@gmail.com>
2026-01-25 04:46:07 +0200

    fix public bug

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,27 +1,41 @@
 import React from "react";
-import { BrowserRouter, createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Services from "./components/Services";
-import Layout from "./components/Layout";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+// import Layout from "./components/Layout";
 
 export default function App() {
-    const Routes = createBrowserRouter([
-        {
-            path: "/",
-            element: <Layout />,
-            children: [
-                { path: "/", element: <Home /> },
-                { path: "about", element: <About /> },
-                { path: "contact", element: <Contact /> },
-                { path: "services", element: <Services /> },
-            ],
-        },
-    ]);
+    // const Routes = createBrowserRouter([
+    //     {
+    //         path: "/",
+    //         element: <Layout />,
+    //         children: [
+    //             { path: "/", element: <Home /> },
+    //             { path: "about", element: <About /> },
+    //             { path: "contact", element: <Contact /> },
+    //             { path: "services", element: <Services /> },
+    //         ],
+    //     },
+    // ]);
+    // return (
+    //     <>
+    //         <RouterProvider router={Routes}></RouterProvider>
+    //     </>
+    // );
     return (
-        <>
-            <RouterProvider router={Routes}></RouterProvider>
-        </>
-    );
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="contact" element={<Contact />} />
+                <Route path="services" element={<Services />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    )
 }
