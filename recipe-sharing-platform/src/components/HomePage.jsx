586e08b804a4062157e9c7b568e82aee6a568482
Eslam Al-Okda <al3kda1999@gmail.com>
2026-02-15 05:20:44 +0200

    Add ingrredients & instructions for RecipeDetails at recipe-platform

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -10,7 +10,7 @@ export default function HomePage() {
     }, [recipes]);
     return (
         <main>
-            <h1 className="text-slate-100 text-2xl bg-slate-800 my-4 p-4 w-fit rounded mx-auto">Welcome to our app</h1>
+            <h1 className="text-slate-100 text-2xl bg-slate-800 my-4 p-4 w-fit rounded mx-auto hover:bg-slate-700 shadow-2xl">Welcome to our app</h1>
             <div className="bg-slate-200 p-6 flex flex-wrap gap-8 ">
                 {recipes.map((r) => (
                     <RecipeCard key={r.id} image={r.image} title={r.title} summary={r.summary} id={r.id} />
--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -1,19 +1,33 @@
-import React, { useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import data from '../data.json'
+import React, { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import data from "../data.json";
 
 export default function RecipeDetail() {
-  const {id} = useParams()
-  const recipe = data.find((r)=>r.id === parseInt(id) )
-  useEffect(() => {
-    console.log(recipe);
-  },[recipe])
-  return (
-    <div className='flex flex-col gap-4 p-4 border border-teal-700 mx-auto'>
-      <img src={recipe.image} alt="" className='w-64 ' />
-      <h2 className='text-blue-500 '>{ recipe.title}</h2>
-      <p className=''>{ recipe.details}</p>
-      <Link to={'/'} className='border border-blue-500 bg-slate-600 text-white w-fit rounded p-1'>go back</Link>
-    </div>
-  )
+    const { id } = useParams();
+    const recipe = data.find((r) => r.id === parseInt(id));
+    useEffect(() => {
+        console.log(recipe);
+    }, [recipe]);
+    return (
+        <div className="flex flex-col gap-4 p-4 border border-teal-700 mx-auto">
+            <img src={recipe.image} alt="" className="w-64 " />
+            <h2 className="text-blue-500 ">{recipe.title}</h2>
+            <p className="">{recipe.details}</p>
+            <h3 className="text-2xl ">Ingredients</h3>
+            <ul className="border border-blue-200 rounded p-4 ">
+                {recipe.ingredients.map((i) => (
+                    <li className="my-1 bg-slate-100">{i}</li>
+                ))}
+            </ul>
+            <h3 className="text-2xl">Instructions</h3>
+            <ol className="list-decimal">
+                {recipe.instructions.map((i) => (
+                    <li className="p-1 bg-slate-100 my-1">{i}</li>
+                ))}
+            </ol>
+            <Link to={"/"} className="border border-blue-500 bg-slate-600 text-white w-fit rounded p-1">
+                go back
+            </Link>
+        </div>
+    );
 }
